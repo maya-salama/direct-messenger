2559f4c05c4ca53480c81d8fb7f8aa4d39e11727
maya salama <salamam2@uci.edu>
2026-06-02 13:15:28 -0700

    Add empty starter files for a4

---
+++ ds_protocol.py
@@ -0,0 +1,47 @@
+# ds_protocol.py
+
+# Starter code for assignment 3 in ICS 32 Programming
+# with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Maya Salama
+# salamam2@uci.edu
+# 74793795
+
+import json
+from collections import namedtuple
+
+# Namedtuple to hold the values retrieved from json messages.
+
+DataTuple = namedtuple('DataTuple', ['status', 'token'])
+
+
+def extract_json(json_msg: str) -> DataTuple:
+    '''
+  Call the json.loads function on a json string
+  and convert it to a DataTuple object
+
+    '''
+    try:
+        json_obj = json.loads(json_msg)
+        status = json_obj['response']['type']
+        token = json_obj['response'].get('token')
+        return DataTuple(status, token)
+    except json.JSONDecodeError:
+        print("Json cannot be decoded.")
+
+
+def format_join(username, password):
+    d = {"join": {"username": username, "password": password, "token": ""}}
+    return json.dumps(d)
+
+
+def format_post(token, entry, timestamp):
+    d = {"token": token, "post": {"entry": entry, "timestamp": timestamp}}
+    return json.dumps(d)
+
+
+def format_bio(token, bio, timestamp):
+    d = {"token": token, "bio": {"entry": bio, "timestamp": timestamp}}
+    return json.dumps(d)
