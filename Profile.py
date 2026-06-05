86ce5ee97783bd0cf105dd99a18281899714d058
maya salama <salamam2@uci.edu>
2026-06-04 18:39:48 -0700

    Extend Profile with friends and messages for local storage

--- Profile.py
+++ Profile.py
@@ -110,6 +110,9 @@ class Profile:
         self.password = password  # REQUIRED
         self.bio = ''            # OPTIONAL
         self._posts = []         # OPTIONAL
+        self.friends = []
+        self.messages = {}
+
 
     """
 
@@ -206,6 +209,8 @@ class Profile:
                 for post_obj in obj['_posts']:
                     post = Post(post_obj['entry'], post_obj['timestamp'])
                     self._posts.append(post)
+                self.friends = obj.get("friends", [])
+                self.messages = obj.get("messages", {})
                 f.close()
             except Exception as ex:
                 raise DsuProfileError(ex)
